78e245bd59ec8c0f5ab47fc6a9646b08ae5ef231
Eric Chau <echau28@my.bcit.ca>
2023-02-24 12:31:46 -0800

    Added API's for firebase - EC

---
+++ scripts/firebaseAPI_team19.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+  apiKey: "***",
+  authDomain: "***",
+  projectId: "***",
+  storageBucket: "****",
+  messagingSenderId: "****",
+  appId: "****"
+};
+
+//--------------------------------------------
+// initialize the Firebase app
+// initialize Firestore database if using it
+//--------------------------------------------
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
