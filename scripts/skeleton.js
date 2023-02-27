0d1afe82949ec0b15a0ddf15ba55d453d4bf41f0
Eric Chau <echau28@my.bcit.ca>
2023-02-26 22:16:21 -0800

    Added additional pages - EC

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -2,5 +2,7 @@
 function loadSkeleton() {
     $('#navbar').load('../text/navbar.html');
     $('#footer').load('../text/footer.html');
+    $('#vehicle_info').load('../text/vehicle_info.html')
+    $('#vehicle_selector').load('../text/vehicle_selector.html')
 }
 loadSkeleton();
