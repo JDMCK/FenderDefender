4398097479556636bf3cda71f99e4b3501ebfeb7
Jesse McKenzie <jesse@jessemckenzie.com>
2023-02-27 21:30:20 -0800

    added a working map. will get the details (hopefully) working tomorrow.

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -1,7 +1,7 @@
 
 function loadSkeleton() {
     $('#navbar').load('../text/navbar.html');
-    $('#footer').load('../text/footer.html');
-    $('#searchbar').load('../text/searchbar.html')
+    $('#searchbar').load('../text/searchbar.html');
+    $('#map').load('../text.googleMap.html');
 }
 loadSkeleton();
