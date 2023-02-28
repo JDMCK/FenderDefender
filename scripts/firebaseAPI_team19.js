a82d8ab00d39847a544cf96165c3ccdfb8209286
Eric Chau <echau28@my.bcit.ca>
2023-02-28 15:11:43 -0800

    consolidate vehicle pages + added favicon's - Eric Chau

--- scripts/firebaseAPI_team19.js
+++ scripts/firebaseAPI_team19.js
@@ -1,10 +1,10 @@
 var firebaseConfig = {
-  apiKey: "***",
-  authDomain: "***",
-  projectId: "***",
-  storageBucket: "****",
-  messagingSenderId: "****",
-  appId: "****"
+  apiKey: "AIzaSyCcjyl44eHQwAo8oEcOBbxZI5dA3BiTNcQ",
+  authDomain: "project-5895497555980020212.firebaseapp.com",
+  projectId: "project-5895497555980020212",
+  storageBucket: "project-5895497555980020212.appspot.com",
+  messagingSenderId: "490172205289",
+  appId: "1:490172205289:web:5ffc801eeb70a3c4050878"
 };
 
 //--------------------------------------------
